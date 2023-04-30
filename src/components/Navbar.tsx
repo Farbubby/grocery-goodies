7b3645c031ddf34670789e1506e725f86e389d67
farbubby <farhanmahbub88@Knights.ucf.edu>
2023-04-29 20:35:59 -0400

    working on the navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,14 @@
 function Navbar() {
   return (
-    <div>
-      Navbar here
-    </div>
+    <>
+      <div className="flex w-full h-14 bg-red-400">
+        <div className="flex h-14 items-center gap-x-9">
+          <div>Home</div>
+          <div>About</div>
+          <div>Thingies</div>
+        </div>
+      </div>
+    </>
   )
 }
 
