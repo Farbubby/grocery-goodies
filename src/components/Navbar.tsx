3314719d1001d749c6b1f189a87337d445429c71
farbubby <farhanmahbub88@Knights.ucf.edu>
2023-04-30 12:57:45 -0400

    added elements in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,15 @@
 function Navbar() {
   return (
     <>
-      <div className="flex w-full h-14 bg-red-400">
-        <div className="flex h-14 items-center gap-x-9">
+      <div className="flex w-full h-14 bg-red-400 border-b border-black">
+        <div className="flex p-2 w-14 text-2xl font-bold items-center gap-x-2 mr-auto">
+          <img src="./src/assets/logo.webp" alt="Logo"></img>
+          Store
+        </div>
+        <div className="flex h-14 font-white font-bold items-center gap-x-9 mr-20">
           <div>Home</div>
           <div>About</div>
-          <div>Thingies</div>
+          <div>Purchase</div>
         </div>
       </div>
     </>
