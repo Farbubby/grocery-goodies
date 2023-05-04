b4431c151f0afa8aafb6a3405ed4d6188c6a8e73
farbubby <farhanmahbub88@Knights.ucf.edu>
2023-05-04 11:57:00 -0400

    adding state to food type buttons

--- src/components/FoodTypes.tsx
+++ src/components/FoodTypes.tsx
@@ -1,21 +1,27 @@
-import Menu from "./Menu"
+import { useState } from "react";
+import Menu from "./Menu";
 
 interface Props {
   list: String[];
 }
 
 function FoodTypes({ list }: Props) {
+  const [option, setOption] = useState("");
   const foodList = list.map((items) => (
-    <button className="border border-black rounded-lg px-2 py-0.5 hover:bg-black hover:text-white duration-300">
+    <button className="border border-black rounded-lg px-2 py-0.5 hover:bg-black hover:text-white duration-300" 
+    onClick={(items) => setOption("ok")}>
       {items}
     </button>
   ));
-  const meat = ['Chicken', '$12.00', 'Beef', '$15.00'];
+  const meats = ["Chicken", "$12.00", "Beef", "$15.00"];
+  const vegetables = ["Potato", "$1.50", "Pepper", "$2.00"];
+  const fruits = ["Apple", "$5.00", "Orange", "$3.00"];
+  const drinks = ["Sprite", "$7.25", "Coke", "$8.00"];
 
   return (
     <>
       <div className="mt-4 flex gap-5 ml-2">{foodList}</div>
-      <Menu list={meat}/>
+      <Menu list={drinks} />
     </>
   );
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,18 +1,18 @@
 import Card from "./Card";
 
 interface Props {
-    list: String[];
+  list: String[];
 }
 
-function Menu({list}: Props) {
-    return (
-        <>
-        <div className="md:flex mt-10 ml-2">
-            <Card product={list[0]} price={list[1]} />
-            <Card product={list[2]} price={list[3]} />
-        </div>
-        </>
-    )
+function Menu({ list }: Props) {
+  return (
+    <>
+      <div className="md:flex mt-10 ml-2">
+        <Card product={list[0]} price={list[1]} />
+        <Card product={list[2]} price={list[3]} />
+      </div>
+    </>
+  );
 }
 
-export default Menu
+export default Menu;
