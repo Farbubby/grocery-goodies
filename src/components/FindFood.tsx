88b0f2f053fab5323071e2e40965fe837d3d9168
farbubby <farhanmahbub88@Knights.ucf.edu>
2023-05-05 13:31:14 -0400

    fixed state changes

--- src/components/FindFood.tsx
+++ src/components/FindFood.tsx
@@ -6,10 +6,10 @@ interface Props {
 }
 
 function FindFood({ list }: Props) {
-  const [option, setOption] = useState("");
-  const foodList = list.map((items) => (
-    <button className="border border-black rounded-lg px-2 py-0.5 hover:bg-black hover:text-white duration-300" 
-    onClick={(items) => setOption("ok")}>
+  const [option, setOption] = useState(-1);
+  const foodList = list.map((items, index) => (
+    <button className={option === index ? "border border-black rounded-lg px-2 py-0.5 bg-black text-white duration-300" : "border border-black rounded-lg px-2 py-0.5 hover:bg-black hover:text-white duration-300"} 
+    onClick={(items) => setOption(index)}>
       {items}
     </button>
   ));
