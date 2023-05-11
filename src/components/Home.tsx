dce1ddc242a3f6b2918770f457847574efba1c3a
farbubby <farhanmahbub88@Knights.ucf.edu>
2023-05-11 10:37:18 -0400

    created a Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,9 @@
 function Home() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center mt-10">
         <h1 className="text-4xl font-bold">Welcome to Food App</h1>
-        <p className="text-xl">Find your favorite food here</p>
+        <p className="text-xl">Find your groceries here</p>
       </div>
     </>
   );
