8b3b60fe65182e482962e46289b34dfb118e74f5
farbubby <farhanmahbub88@Knights.ucf.edu>
2023-05-11 20:27:34 -0400

    state now uses string instead of number

--- src/components/FoodSelection.tsx
+++ src/components/FoodSelection.tsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 
 interface Props {
-  list: String[];
+  list: string[];
 }
 
 function FoodSelection({ list }: Props) {
-  const [option, setOption] = useState(0);
+  const [option, setOption] = useState("Meats");
   let str = "border border-black rounded-lg px-2 py-0.5 duration-300";
-  const foodList = list.map((items, index) => (
+  const foodList = list.map((items) => (
     <button
       className={
-        option === index
+        option === items
           ? str.concat(" bg-black text-white")
           : str.concat(" hover:bg-black hover:text-white")
       }
-      onClick={() => setOption(index)}
+      onClick={() => setOption(items)}
     >
       {items}
     </button>
@@ -22,7 +22,7 @@ function FoodSelection({ list }: Props) {
 
   return (
     <>
-      <div className="mt-4 flex gap-5 ml-2">{foodList}</div>
+      <div className="mt-4 flex flex-row gap-5 ml-2">{foodList}</div>
     </>
   );
 }
