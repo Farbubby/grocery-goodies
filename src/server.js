23505e3073d7b0d159ade08f9c80430c886e3750
farbubby <farhanmahbub88@Knights.ucf.edu>
2023-05-19 21:57:45 -0400

    added more items to the grocery list

--- src/server.js
+++ src/server.js
@@ -19,6 +19,23 @@ let foodList = [
   { type: "Vegetable", name: "Potato", price: 0.99 },
   { type: "Vegetable", name: "Tomato", price: 1.99 },
   { type: "Drink", name: "Coke", price: 4.99 },
+  { type: "Drink", name: "Pepsi", price: 4.99 },
+  { type: "Drink", name: "Sprite", price: 4.99 },
+  { type: "Drink", name: "Fanta", price: 4.99 },
+  { type: "Drink", name: "Mountain Dew", price: 4.99 },
+  { type: "Fruit", name: "Watermelon", price: 5.99 },
+  { type: "Fruit", name: "Pineapple", price: 3.99 },
+  { type: "Fruit", name: "Mango", price: 2.99 },
+  { type: "Fruit", name: "Strawberry", price: 2.99 },
+  { type: "Vegetable", name: "Cucumber", price: 1.99 },
+  { type: "Vegetable", name: "Onion", price: 0.99 },
+  { type: "Meat", name: "Salmon", price: 7.99 },
+  { type: "Meat", name: "Tuna", price: 6.99 },
+  { type: "Vegetable", name: "Broccoli", price: 1.99 },
+  { type: "Vegetable", name: "Cabbage", price: 1.99 },
+  { type: "Vegetable", name: "Spinach", price: 1.99 },
+  { type: "Vegetable", name: "Lettuce", price: 1.99 },
+  { type: "Vegetable", name: "Pepper", price: 1.99 },
 ];
 
 app.get("/meats", (req, res) => {
