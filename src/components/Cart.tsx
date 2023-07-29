ede380f18cdb17290213bc8fc93725f6686807b8
farbubby <farhan.mahbub@ucf.edu>
2023-07-28 23:13:38 -0400

    designed the cart list with css

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -7,6 +7,9 @@ interface Props {
 }
 
 function Cart({ state, close }: Props) {
+  const buttonCSS =
+    " px-2 border border-green-300 rounded-full hover:text-black hover:bg-green-300 duration-200";
+
   const queryClient = useQueryClient();
 
   let toggle = state
@@ -45,16 +48,24 @@ function Cart({ state, close }: Props) {
 
   const list = query.data.map((item: any) => (
     <div className="flex flex-row gap-4">
-      <div className="flex flex-col border-r px-4 border-green-300 items-center">
-        <div>
-          {item.name} x{item.amount} - ${(item.price * item.amount).toFixed(2)}
+      <button onClick={() => remove.mutate(item)}>X</button>
+      <div className="flex flex-col px-4 py-2 border-l border-green-300 gap-2">
+        <div className="flex flex-row gap-24 max-w-xl border-b border-green-300 pb-1">
+          <div className="flex flex-col">
+            <div>{item.name}</div>
+            <div>x{item.amount}</div>
+          </div>
+          <div>${(item.price * item.amount).toFixed(2)}</div>
         </div>
-        <div className="flex flex-row gap-4">
-          <button onClick={() => addOne.mutate(item)}>+</button>
-          <button onClick={() => removeOne.mutate(item)}>-</button>
+        <div className="flex flex-row gap-2">
+          <button className={buttonCSS} onClick={() => addOne.mutate(item)}>
+            +
+          </button>
+          <button className={buttonCSS} onClick={() => removeOne.mutate(item)}>
+            -
+          </button>
         </div>
       </div>
-      <button onClick={() => remove.mutate(item)}>X</button>
     </div>
   ));
 
@@ -68,9 +79,7 @@ function Cart({ state, close }: Props) {
         <div className="flex flex-row-reverse p-8 text-xl mb-8">
           <button onClick={() => close()}>X</button>
         </div>
-        <div className="flex flex-col justify-center items-center gap-4">
-          {list}
-        </div>
+        <div className="flex flex-col justify-center px-8 gap-4">{list}</div>
       </div>
     </>
   );
