5960c40ac1f4257cb64c9a4436b5dfdae96ff144
farbubby <farhan.mahbub@ucf.edu>
2023-08-01 18:29:40 -0400

    abstracted segment of code into react component

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,6 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { getCart, deleteItem, addItem, removeItem } from "../api/functions";
+import Item from "./Item";
 
 interface Props {
   state: boolean;
@@ -21,52 +22,12 @@ function Cart({ state, close }: Props) {
     queryFn: () => getCart(),
   });
 
-  const addOne = useMutation({
-    mutationFn: (data: any) => addItem(data),
-    onSuccess: () => {
-      queryClient.invalidateQueries(["cart"]);
-    },
-  });
-
-  const removeOne = useMutation({
-    mutationFn: (data: any) => removeItem(data),
-    onSuccess: () => {
-      queryClient.invalidateQueries(["cart"]);
-    },
-  });
-
-  const remove = useMutation({
-    mutationFn: (data: any) => deleteItem(data),
-    onSuccess: () => {
-      queryClient.invalidateQueries(["cart"]);
-    },
-  });
-
   if (query.isLoading) return <div>Loading...</div>;
   if (query.isError)
     return <div>An error has occurred: {JSON.stringify(query.error)}</div>;
 
   const list = query.data.map((item: any) => (
-    <div className="flex flex-row gap-4">
-      <button onClick={() => remove.mutate(item)}>X</button>
-      <div className="flex flex-col px-4 py-2 border-l border-green-300 gap-2">
-        <div className="flex flex-row gap-24 max-w-xl border-b border-green-300 pb-1">
-          <div className="flex flex-col">
-            <div>{item.name}</div>
-            <div>x{item.amount}</div>
-          </div>
-          <div>${(item.price * item.amount).toFixed(2)}</div>
-        </div>
-        <div className="flex flex-row gap-2">
-          <button className={buttonCSS} onClick={() => addOne.mutate(item)}>
-            +
-          </button>
-          <button className={buttonCSS} onClick={() => removeOne.mutate(item)}>
-            -
-          </button>
-        </div>
-      </div>
-    </div>
+    <Item item={item} />
   ));
 
   return (
---
+++ src/components/Item.tsx
@@ -0,0 +1,63 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteItem, addItem, removeItem } from "../api/functions";
+
+interface Props {
+  item: {
+    name: string;
+    price: number;
+    amount: number;
+  };
+}
+
+function Item({ item }: Props) {
+  const buttonCSS =
+    " px-2 border border-green-300 rounded-full hover:text-black hover:bg-green-300 duration-200";
+
+  const queryClient = useQueryClient();
+
+  const addOne = useMutation({
+    mutationFn: (data: any) => addItem(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["cart"]);
+    },
+  });
+
+  const removeOne = useMutation({
+    mutationFn: (data: any) => removeItem(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["cart"]);
+    },
+  });
+
+  const remove = useMutation({
+    mutationFn: (data: any) => deleteItem(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["cart"]);
+    },
+  });
+
+  return (
+    <div className="flex flex-row gap-4">
+      <button onClick={() => remove.mutate(item)}>X</button>
+      <div className="flex flex-col px-4 py-2 border-l border-green-300 gap-2">
+        <div className="flex flex-row gap-24 max-w-xl border-b border-green-300 pb-1">
+          <div className="flex flex-col">
+            <div>{item.name}</div>
+            <div>x{item.amount}</div>
+          </div>
+          <div>${(item.price * item.amount).toFixed(2)}</div>
+        </div>
+        <div className="flex flex-row gap-2">
+          <button className={buttonCSS} onClick={() => addOne.mutate(item)}>
+            +
+          </button>
+          <button className={buttonCSS} onClick={() => removeOne.mutate(item)}>
+            -
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Item;
